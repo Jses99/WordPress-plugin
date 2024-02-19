5e6e587f49c8b5f983733007619e1af415bd2b36
Jses99 <70154064+Jses99@users.noreply.github.com>
2024-02-19 14:15:25 +0200

    name change

---
+++ customer-feedback.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Customer Feedback
+Description: Plugin to show customer feedback
+Author: Jonna Satta
+*/
+ 
+function cfplugin_setup_menu() {
+    add_menu_page();
+}
+
+?>
